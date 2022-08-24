4556a4308afabe4d4582abd85133553146ca00b1
Lapenok <osharapova@yandex.com>
2022-08-25 01:45:31 +0300

    change const port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 
 app.use(express.static(__dirname + "/dist"));
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT || PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
